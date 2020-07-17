4ba7db4b76c98546f67b1b1d9bdd806996e793b9
Damien Sawyer <damiensawyer@gmail.com>
2020-07-17 23:40:56 +0100

    dockerfile

--- up.sh
+++ up.sh
@@ -7,6 +7,13 @@ else
     docker rm -f nginxdeb
 fi
 
+
+if [ "$(docker image ls docker_nginxstud.debianbase)" ]; then
+    echo "removing container nginxdeb"
+    docker image rm docker_nginxstud.debianbase -f
+fi
+
+
 docker-compose -f "docker/docker-compose.yml" down ## call down to get new volume https://github.com/docker/compose/issues/4476
 docker-compose -f "docker/docker-compose.yml" --no-ansi up -d --force-recreate --remove-orphans
 docker ps  -a
